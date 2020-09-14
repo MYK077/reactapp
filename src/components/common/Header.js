4b368096e5c79d2778f964c00fc888189c324815
Mayank <myk.vit@gmail.com>
2020-09-14 14:30:46 -0500

    adding header and courses page

---
+++ src/components/common/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <nav>
+      <a href="/">Homepage</a> | <a href="/courses">Courses</a> |{" "}
+      <a href="/about">AboutPage</a>
+    </nav>
+  );
+}
+
+export default Header;
