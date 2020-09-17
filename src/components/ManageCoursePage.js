bc8a1cd842705cf0b9ab3c9b0c0fe5261aa562e8
Mayank <myk.vit@gmail.com>
2020-09-17 16:32:28 -0500

    adding react toastify for notifications when data is saved

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
+import { toast } from "react-toastify";
 
 const ManageCoursePage = (props) => {
   const [course, setCourse] = useState({
@@ -17,10 +19,23 @@ const ManageCoursePage = (props) => {
     };
     setCourse(updatedCourse);
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    courseApi.saveCourse(course).then(() => {
+      props.history.push("/courses");
+      toast.success("course saved!");
+    });
+  }
+
   return (
     <>
       <h2>Manage Course</h2>
-      <CourseForm course={course} onChange={handleChange} />
+      <CourseForm
+        course={course}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     </>
   );
 };
